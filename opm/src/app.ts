863ad9c55d0247232dff1ce6a27ab97f065544ae
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-03-25 15:53:11 -0400

    some config stuffs

--- opm/src/app.ts
+++ opm/src/app.ts
@@ -1,35 +1,40 @@
-import dgram from 'node:dgram';
-
+// the usual gang of idiots
+import * as path from "node:path";
+import * as fs from "node:fs";
+const astext = (x: string) => fs.readFileSync(path.normalize(x), { encoding: "utf8" });
+const asnumber = (x: string) => Number(astext(x));
 const cog = console.log;
 
+import dgram from 'node:dgram';
 const socket = dgram.createSocket('udp6');
 
 socket.on('listening', ()=>{
 	const address = socket.address();
-	cog(`socket listening: address is ${address.address} and port is ${address.port}`);
-});
-
-socket.on('connect', ()=>{
-	const remote = socket.remoteAddress();
-	cog(`connect ${remote.address} with port ${remote.port}`);
+	cog(`listening [${address.address}]${address.port}`);
 });
 
 socket.on('message', (msg, rinfo)=>{
-	cog(`we got ${msg} from ${rinfo.address}:${rinfo.port}`);
+	cog(`we got ${msg} from [${rinfo.address}]:${rinfo.port}`);
 });
 
 function tryToSend(port: number, address: string){
-	socket.send(Math.random().toString(), port, address, (err, bytes)=>{
+	const sayWhat = Math.random().toString().padEnd(19, '0');
+	socket.send(sayWhat, port, address, (err, bytes)=>{
 		if (err){
 			cog(`error`, err);
 		} else {
-			cog(`ok`, bytes);
+			cog(`sending ${sayWhat} to [${address}]:${port}`);
 		}
 	});
 }
 
-const ImDoingMyPart = () => tryToSend(39111, 'nothing');
+// for the purposes of this video, we're gonna use some predefined stuffs
+const theRemotePort = asnumber("notkeys/remote_port.txt");
+const theRemoteAddress = astext("notkeys/remote_addr.txt");
+
+const ImDoingMyPart = () => tryToSend(theRemotePort, theRemoteAddress);
+
+const theListenPort = asnumber("notkeys/listen_port.txt");
+socket.bind(theListenPort);
 
 setInterval(ImDoingMyPart, 300);
-
-socket.bind(39111);
