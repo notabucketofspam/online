04aae3c23a388e35f900ced893fbf2cf0e20ec9f
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-03-28 16:32:37 -0400

    paying attention to local http

--- base/src/express_app.ts
+++ base/src/express_app.ts
@@ -368,60 +368,48 @@ async function getPunchHtml(req: Request, res: Response){
 	res.sendFile(path.join(__dirname, "..", 'html','punch.html'));
 }
 
-function rui (k: number) {
-  return Math.trunc(Math.random() * k);
-}
-
-function randomPunchPort(){
-	return (39100 + rui(500));
+interface Punch {
+	// the IP address for someone
+	addr: string;
+	// this is the port for the service that client wants to advertise (ex: 2302)
+	port: number;
+	// this is the port that we're gonna be punching (ex: 39420)
+	punchPort : number;
+	// this is the name of the server, for display purposes
+	serviceName : string;
+	// which way are we swingin'?
+	flavor:'out'|'in'|'stop';
 }
 
 async function postPunchAd(req: Request, res: Response){
-	const expireTime = 600;
+	// absurdly long just for testing
+	const expireTime = 8e8;
 
 	try{
 		if (typeof req?.session?.username === 'string'){
-			// know the Redis key
-			const ad_username = req.session.username;
-			const redKeycard = `PUNCH:${ad_username}`;
 
-			// know the hash field
-			const h_field = `[${req.body?.ip}]:${req.body?.port}`;
+			const punch = req.body as Punch;
+
+			// redis hash key / field / value
+			const redKeycard = `PUNCH:${req.session.username}`;
+			const hashField = `${punch.addr},${punch.port}`;
+			const hashValue = `${punch.punchPort},${punch.serviceName}`;
 
 			// check to see if we're just doing a refresh
-			const httl = await redisClient.hTTL(redKeycard, h_field);
-			if (httl && httl[0] && httl[0] > 0){
-				// we're just gonna refresh the ttl
-				const hexpire_ok = await redisClient.hExpire(redKeycard, h_field, expireTime);
-				if (hexpire_ok[0]){
-					res.status(200).json({msg:hexpire_ok[0]});
-					return;
-				}
-			}
-			// we gotta put in a new punch
-			// check to make sure that this port isnt already being used by punch
-			let p_port = randomPunchPort();
-			const hvals = await redisClient.hVals(redKeycard);
-			goto_top: while(1){
-				for (const val of hvals) {
-					let just_the_port = val.split(',')[0];
-					if (Number(just_the_port) === p_port){
-						p_port = randomPunchPort();
-						continue goto_top;
-					}
-				}
-				break goto_top;
-			}
-			
-			// know the hash value
-			const ad_name = req.body?.sv_name;
-			const h_value = `${p_port},${ad_name}`;
-			// put it together
-			const ad_hash: Record<string, string> = {};
-			ad_hash[h_field] = h_value;
+			//const httl = await redisClient.hTTL(redKeycard, hashField);
+			//if (httl && httl[0] && httl[0] > 0){
+			//	// we're just gonna refresh the ttl
+			//	const hexpire_ok = await redisClient.hExpire(redKeycard, hashField, expireTime);
+			//	if (hexpire_ok[0]){
+			//		res.status(200).json({msg:hexpire_ok[0]});
+			//		return;
+			//	}
+			//}
 
+			// for right now, we're gonna assume that we can't do refreshes
+			
 			// put it right in the machine
-			const hsetex_ok = await redisClient.hSetEx(redKeycard, ad_hash, {expiration:{type:'EX',value:expireTime}});
+			const hsetex_ok = await redisClient.hSetEx(redKeycard, {[hashField]: hashValue}, {expiration:{type:'EX',value:expireTime}});
 			res.status(200).json({msg:hsetex_ok});
 		} else {
 			res.status(500).json({msg:"problem with req.session"});
@@ -438,11 +426,10 @@ async function getPunchList(req: Request, res: Response){
 
 		// for right now, only show punches from the same user
 		const punchKeys = await redisClient.keys(`PUNCH:${req?.session?.username}`);
+
 		const resbody: Record<string, Record<string, string>> = {};
 		for (const key of punchKeys){
 			const hash_real = await redisClient.hGetAll(key);
-			//console.log(key);
-			//console.log(hash_real);
 			resbody[key] = hash_real;
 		}
 		res.status(200).json(resbody);
--- opm/app.ts
+++ opm/app.ts
@@ -5,36 +5,158 @@ const astext = (x: string) => fs.readFileSync(path.normalize(x), { encoding: "ut
 const asnumber = (x: string) => Number(astext(x));
 const cog = console.log;
 
+// ===========================================================
+// http server
+import http from "node:http";
+import net from "node:net";
 import dgram from 'node:dgram';
-const socket = dgram.createSocket('udp6');
+const server = http.createServer({noDelay:true});
 
-socket.on('listening', ()=>{
-	const address = socket.address();
-	cog(`listening [${address.address}]:${address.port}`);
-});
-
-socket.on('message', (msg, rinfo)=>{
-	cog(`we got ${msg} from [${rinfo.address}]:${rinfo.port}`);
-});
-
-function tryToSend(port: number, address: string){
-	const sayWhat = Math.random().toString().padEnd(20, '0');
-	socket.send(sayWhat, port, address, (err, bytes)=>{
-		if (err){
-			cog(`error`, err);
-		} else {
-			cog(`sending ${sayWhat} to [${address}]:${port}`);
-		}
-	});
+interface Punch {
+	// the IP address for someone
+	addr: string;
+	// this is the port for the service that client wants to advertise (ex: 2302)
+	port: number;
+	// this is the port that we're gonna be punching (ex: 39420)
+	punchPort : number;
+	// this is the name of the server, for display purposes
+	serviceName : string;
+	// which way are we swingin'?
+	flavor:'out'|'in'|'stop';
 }
 
-// for the purposes of this video, we're gonna use some predefined stuffs
-const theRemotePort = asnumber("notkeys/remote_port.txt");
-const theRemoteAddress = astext("notkeys/remote_addr.txt");
+const sockets = new Map<number, dgram.Socket>();
 
-const ImDoingMyPart = () => tryToSend(theRemotePort, theRemoteAddress);
+let someInterval : NodeJS.Timeout | null = null;
 
-const theListenPort = asnumber("notkeys/listen_port.txt");
-socket.bind(theListenPort);
+server.on('request', async (req, res)=>{
+	if (req.method === "POST") {
+    req.setEncoding("utf8");
+    let somedata = "";
 
-setInterval(ImDoingMyPart, 2000);
+		const ondata = (chunk:any)=>{
+      somedata += chunk;
+    };
+    req.on("data", ondata);
+		req.once('end', async ()=>{
+			req.off('data', ondata);
+
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			res.setHeader('Access-Control-Allow-Origin', '*');
+
+			// do stuff with the data
+			const punch:Punch = JSON.parse(somedata);
+			cog(somedata);
+
+			// do we need to make a new socket?
+			let socketPort : number;
+			let socket = sockets.get(punch.punchPort);
+			if (typeof socket === 'undefined'){
+				// no socket already, so we gotta make a new one
+				const fam = net.isIPv6(punch.addr) ? 'udp6' : 'udp4';
+				socket = await createSocket(fam);
+				socketPort = socket.address().port;
+				sockets.set(socketPort, socket);
+			} else {
+				// we already have a socket
+				socketPort = socket.address().port;
+			}
+
+			if (punch.flavor === 'in') {
+				// the customer just wants to know what our socket port is
+				res.write(String(socketPort));
+
+			} else if (punch.flavor === 'out'){
+				// and now we gotta talk to someone outside the home
+				someInterval = setInterval(()=>{
+					tryToSend(socket, socketPort, punch.addr);
+				}, 5000);
+				res.write('OK');
+
+			} else if (punch.flavor === 'stop') {
+				// we want to stop everything
+				if (someInterval) {
+					clearInterval(someInterval);
+				}
+				socket.close();
+				socket.off('error', onSocketError);
+				socket.off('message', onSocketMessage);
+				sockets.delete(socketPort);
+				res.write('OK');
+			}
+			
+			// give the customer back a response
+			res.end();
+		});
+	} else {
+		res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('sorry nothing');
+	}
+});
+
+const httpPort = 39111;
+server.once('listening',()=>{
+	cog(`listen http on ${httpPort}`);
+});
+server.listen(httpPort, 'localhost');
+
+// ============================================================
+// the udp stuff
+
+async function createSocket (family : 'udp4' | 'udp6', bindPort = 0) {
+	const socket = dgram.createSocket(family);
+
+	socket.on('error', onSocketError);
+	socket.on('message', onSocketMessage);
+	socket.once('close', onceSocketClose);
+	// prevent some kinda race condition
+	const promise = new Promise<dgram.Socket>((resolve, reject) => {
+		socket.once('listening', () => {
+			onceSocketListening(socket);
+			resolve(socket);
+		});
+	});
+
+	socket.bind(bindPort);
+	await promise;
+
+	return socket;
+}
+
+function onceSocketListening(socket: dgram.Socket){
+	const address = socket.address();
+	cog(`udp socket listening on ${address.address},${address.port}`);
+}
+
+function onSocketError (err : Error) {
+	console.error(`socket error: ${err}`);
+}
+
+function onSocketMessage (msg : Buffer, rinfo : dgram.RemoteInfo) {	
+	cog(`we got ${msg} from ${rinfo.address},${rinfo.port}`);
+}
+
+function onceSocketClose () {
+	cog(`socket closed`);
+}
+
+function tryToSend(socket: dgram.Socket, destPort: number, destAddr: string){
+	try{
+		const sayWhat = Math.random().toString().padEnd(20, '0');
+		socket.send(sayWhat, destPort, destAddr, (err, bytes)=>{
+			if (err){
+				cog(`error`, err);
+			} else {
+				cog(`sending ${sayWhat} to ${destAddr},${destPort}`);
+			}
+		});
+	} catch(err){
+		console.error(`error`, err);
+	}
+}
+
+//const ImDoingMyPart = () => tryToSend(theRemotePort, theRemoteAddress);
+
+//setInterval(ImDoingMyPart, 2000);
