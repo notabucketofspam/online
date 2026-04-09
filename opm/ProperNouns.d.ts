393e61c9aca182fe5eb6a14ee1e0f2fd1cdb9c64
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-04-09 08:29:45 -0400

    Create ProperNouns.d.ts

---
+++ opm/ProperNouns.d.ts
@@ -0,0 +1,51 @@
+declare module 'ProperNouns' {
+	import dgram from 'node:dgram';
+	
+	/** Info about some kinda hosted service */
+	export interface Punch {
+		/**the IP address for someone */
+		addr: string;
+		/** this is the port for the service that client wants to advertise (ex: 2302) */
+		port: number;
+		/**this is the name of the server, for display purposes */
+		serviceName : string;
+		/**Who posted this?*/
+		username: string;
+	}
+
+	/**info about a WebSocket that's connected to WSBC*/
+	export interface WsClientInfo {
+		ws: WebSocket;
+		services: Punch[];
+		copiumTimer?: NodeJS.Timeout;
+		refreshTimer?: NodeJS.Timeout;
+	}
+
+	export declare interface WsEventData {
+		flavour: 'punch';
+		body: Punch 
+	}
+	export declare interface WsEventData {
+		flavour: 'wire-info';
+		body: WireInfo;
+	}
+	
+	/**More specific connection info*/
+	export interface WireInfo {
+		/**the port for the app that we want to punch for*/
+		app_port: number;
+		/**our punch peer's IP address*/
+		remote_addr: string;
+		/**The punch peer's punch port*/
+		remote_port?: number;
+	}
+
+	/**Two (2) UDP sockets*/
+	export interface UdpPair {
+		/**socket that communicates with the app*/
+		factorio_socket: dgram.Socket;
+		/**The socket that talks to people outside the home.*/
+		punch_socket: dgram.Socket;
+	}
+
+}
