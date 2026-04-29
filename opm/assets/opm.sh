c84b6440c061dcc09a07f22f8b1c4fb027eb9f75
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-04-29 13:32:37 -0400

    Create opm.sh

---
+++ opm/assets/opm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+if [ ! -f "./node" ] || [ ! -x "./node" ]; then
+  echo Gotta download node
+  curl -O "https://waluigi-servebeer.com/dlc/opm/node.xz"
+  xz --decompress --force "node.xz"
+  chmod +x "./node"
+  echo Done with that
+fi
+
+echo Loading...
+curl -s -O "https://waluigi-servebeer.com/dlc/opm/opm.js"
+clear
+
+./node opm.js
+read -p "Press any key to continue . . . "
