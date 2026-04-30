a4aa605769104fcd13657d2fa50e73a9b1d505dc
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-04-29 20:49:39 -0400

    Update opm.sh

--- opm/assets/opm.sh
+++ opm/assets/opm.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-
+nodecmd="node"
 if [ ! $(command -v node) ] && [ ! -x "./node" ]; then
   echo Gotta download node
   curl -O "https://waluigi-servebeer.com/dlc/opm/node.xz"
   xz --decompress --force "node.xz"
   chmod +x "./node"
+  nodecmd="./node"
   echo Done with that
 fi
 
 echo Loading...
 curl -s -O "https://waluigi-servebeer.com/dlc/opm/opm.js"
-clear
 
-./node opm.js
-read -p "Press any key to continue . . . "
+$nodecmd opm.js
