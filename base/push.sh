ed58fed4754368d8a7851fc138991fb8d832092e
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-05-01 19:36:12 -0400

    make it more clear wtf is going on
    also build on push

--- base/push.sh
+++ base/push.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+tsc --build
+echo "tsc OK"
 rsync \
   --compress \
   --recursive \
@@ -8,5 +10,7 @@ rsync \
   --copy-links \
   -e "ssh -i \"./notkeys/key\"" \
   ./ ubuntu@193.122.154.50:/httpd/online/
+echo "rsync OK"
 ssh -i "./notkeys/key" ubuntu@193.122.154.50 "pm2 restart online > /dev/null"
 echo "Done"
+sleep 2
