1acb966613a3feef9459bcd32fad8d6a0d389928
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-05-07 12:09:02 -0400

    Update __push.bat

--- opm/__push.bat
+++ opm/__push.bat
@@ -1,11 +1,11 @@
 @ECHO off
-TITLE _push.bat
+TITLE %~nx0
 
 REM build opm
-CMD /C "tsc"
+CMD /K "tsc&& EXIT"
 
 REM push to remote
-SET what_files=assets/opm.bat assets/services.json dist/opm.js assets/opm-node.bat assets/opm.sh
+SET what_files=assets/opm.bat assets/services.json dist/opm.js assets/product-key.json assets/opm.sh
 pscp %what_files% OCI-cool:/httpd/dlc/opm/
 
 ECHO DONE
