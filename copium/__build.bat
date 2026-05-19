ceec186c7dbedbac3fcc62eef3dbcc3a36cd504d
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-05-19 16:45:11 -0400

    Update __build.bat

--- copium/__build.bat
+++ copium/__build.bat
@@ -1,29 +1,17 @@
 @ECHO off
-TITLE copium build
+TITLE copium %~nx0
 
 REM Windows
-REM SET os_machine=x86_64 aarch64
-REM FOR /F %%G in ("%os_machine%") DO (
-  REM zig c++ -O3 copium.cpp -target %%G-windows-msvc -o build/copium-windows-%%G.exe
-REM )
 zig c++ -O3 copium.cpp -target x86_64-windows-gnu -o build/copium-windows-x86_64.exe -lws2_32
-REM zig c++ -O3 copium.cpp -target %%G-windows-msvc -o build/copium-windows-%%G.exe
+zig c++ -O3 copium.cpp -target aarch64-windows-gnu -o build/copium-windows-aarch64.exe -lws2_32
+DEL build\*.pdb
 
 REM Linux
-REM SET os_machine=x86_64 aarch64
-REM FOR /F %%G in ("%os_machine%") DO (
-  REM zig c++ -O3 copium.cpp -target %%G-linux-musl -o build/copium-linux-%%G -s
-REM )
-zig c++ -O3 copium.cpp -target x86_64-linux-musl -o build/copium-linux-x86_64 -s
-zig c++ -O3 copium.cpp -target aarch64-linux-musl -o build/copium-linux-aarch64 -s
+zig c++ -O3 copium.cpp -target x86_64-linux-gnu -o build/copium-linux-x86_64 -s
+zig c++ -O3 copium.cpp -target aarch64-linux-gnu -o build/copium-linux-aarch64 -s
 
 REM MacOS
-REM SET os_machine=x86_64 aarch64
-REM FOR /F %%G in ("%os_machine%") DO (
-  REM zig c++ -O3 copium.cpp -target %%G-macos-none -o build/copium-darwin-%%G
-REM )
 zig c++ -O3 copium.cpp -target x86_64-macos-none -o build/copium-darwin-x86_64
 zig c++ -O3 copium.cpp -target aarch64-macos-none -o build/copium-darwin-aarch64
 
-REM timeout /t 10
-pause>nul
+ECHO BUILD OK
