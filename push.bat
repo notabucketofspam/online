ef2faeaf255f5d298ecbb547995021a45857b55c
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-05-19 18:27:12 -0400

    Update push.bat

--- push.bat
+++ push.bat
@@ -1,11 +1,22 @@
 @ECHO off
 TITLE %~nx0
+
 ECHO Pushing base...
 CD base
 CALL mexec "./push.sh"
 ECHO.
 CD ..
+
 ECHO Pushing opm...
 CD opm
 CALL __push.bat
+ECHO.
+CD ..
+
+ECHO Pushing copium...
+CD copium
+CALL __push.bat
+ECHO.
+CD ..
+
 timeout /t 10
