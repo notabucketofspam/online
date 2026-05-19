bd5ea488ae9b1e255932076fe2d58bc4784c6947
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-05-19 17:44:39 -0400

    save them with nicer names

--- copium/__build.bat
+++ copium/__build.bat
@@ -2,16 +2,16 @@
 TITLE copium %~nx0
 
 REM Windows
-zig c++ -O3 copium.cpp -target x86_64-windows-gnu -o build/copium-windows-x86_64.exe -lws2_32
-zig c++ -O3 copium.cpp -target aarch64-windows-gnu -o build/copium-windows-aarch64.exe -lws2_32
+zig c++ -O3 copium.cpp -target x86_64-windows-gnu -o build/copium-Windows_NT-x86_64.exe -lws2_32
+zig c++ -O3 copium.cpp -target aarch64-windows-gnu -o build/copium-Windows_NT-aarch64.exe -lws2_32
 DEL build\*.pdb
 
 REM Linux
-zig c++ -O3 copium.cpp -target x86_64-linux-gnu -o build/copium-linux-x86_64 -s
-zig c++ -O3 copium.cpp -target aarch64-linux-gnu -o build/copium-linux-aarch64 -s
+zig c++ -O3 copium.cpp -target x86_64-linux-gnu -o build/copium-Linux-x86_64 -s
+zig c++ -O3 copium.cpp -target aarch64-linux-gnu -o build/copium-Linux-aarch64 -s
 
 REM MacOS
-zig c++ -O3 copium.cpp -target x86_64-macos-none -o build/copium-darwin-x86_64
-zig c++ -O3 copium.cpp -target aarch64-macos-none -o build/copium-darwin-aarch64
+zig c++ -O3 copium.cpp -target x86_64-macos-none -o build/copium-Darwin-x86_64
+zig c++ -O3 copium.cpp -target aarch64-macos-none -o build/copium-Darwin-aarch64
 
 ECHO BUILD OK
