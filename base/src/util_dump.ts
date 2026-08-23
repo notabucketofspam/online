49e6e24db3f6228acf3bd7bf0df12a967fd62d5c
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-08-23 19:56:23 -0400

    Update util_dump.ts

--- base/src/util_dump.ts
+++ base/src/util_dump.ts
@@ -1,5 +1,9 @@
 import fs from "node:fs";
 import path from "node:path";
+import crypto from "node:crypto";
 export function astext(x: string) {
-    return fs.readFileSync(path.normalize(x), { encoding: "utf8" });
+  return fs.readFileSync(path.normalize(x), {encoding: "utf8"});
+}
+export function rember<T>(arr: T[]): T {
+  return arr[crypto.randomInt(arr.length)]!;
 }
