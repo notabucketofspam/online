68e0e97aeeee32efb91fffadc416f67a8b7df1c6
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-09-03 11:32:19 -0400

    CALL

--- redis-server.bat
+++ redis-server.bat
@@ -1,3 +1,3 @@
 @echo off
 REM follow build directions in solutes/redis.md
-mexec.bat "~/redis/src/redis-server.exe ~/redis/redis.conf"
+CALL mexec.bat "~/redis/src/redis-server.exe ~/redis/redis.conf"
