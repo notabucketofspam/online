44020381566fcc0fa41e5d50988b74fd20573da4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-09-05 10:45:40 -0400

    Update app.ts

--- base/src/app.ts
+++ base/src/app.ts
@@ -8,10 +8,10 @@ import "./product_key";
 import "./livekit";
 import "./cdi/banquet";
 import rt_baltimore from "./dmv/baltimore";
-import rt_goobo from "./interstate";
+import {rt_goobo} from "./interstate";
 
 express_app.use("/api/dmv", rt_baltimore);
-express_app.use("/api/goobo", rt_goobo);
+express_app.use("/goobo", rt_goobo);
 
 import {astext} from "./util_dump";
 
